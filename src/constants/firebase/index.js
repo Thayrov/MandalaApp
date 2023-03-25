bb7136733f9b37924a5d23d1f84e85f0f2c19528
thayrov <thayrovg@gmail.com>
2023-03-25 14:43:20 -0600

    add firebase constants and register project in firebase for rtdb and auth

---
+++ src/constants/firebase/index.js
@@ -0,0 +1,4 @@
+export const API_KEY = '';
+export const REALTIME_DB_URL = 'https://mandalaapp-dd5f1-default-rtdb.firebaseio.com/';
+export const URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+export const URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
