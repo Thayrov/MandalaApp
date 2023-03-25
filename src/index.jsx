b495aa525bcc23c9294eca178a1ae606a5503648
thayrov <thayrovg@gmail.com>
2023-03-25 14:13:48 -0600

    add expo-font and fonts for project

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,21 @@
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
 
+import { THEME } from './constants/theme';
 import styles from './styles';
+import { useFonts } from 'expo-font';
 
 const App = () => {
+    const [loaded] = useFonts({
+        'Caveat-variable': require('../assets/fonts/Caveat-VariableFont_wght.ttf'),
+        'Rancho-Regular': require('../assets/fonts/Rancho-Regular.ttf'),
+    });
+    if (!loaded) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color={THEME.colors.SecondaryColor} />
+            </View>
+        );
+    }
     return (
         <View style={styles.container}>
             <Text>MandalaApp!</Text>
