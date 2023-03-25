3402f601ab10a14c3ff626363a17f0330b765e07
thayrov <thayrovg@gmail.com>
2023-03-25 16:25:38 -0600

    change mock data structure

---
+++ src/constants/data/categories.js
@@ -0,0 +1,14 @@
+import { THEME } from '../theme';
+
+export const CATEGORIES = [
+    {
+        id: 1,
+        title: 'Fondo Blanco',
+        color: THEME.colors.PrimaryColor,
+    },
+    {
+        id: 2,
+        title: 'Fondo Negro',
+        color: THEME.colors.SecondaryColor,
+    },
+];
---
+++ src/constants/data/mandalas.js
@@ -0,0 +1,79 @@
+export const MANDALAS = [
+    {
+        id: 1,
+        categoryId: 1,
+        title: 'Blue Ocean',
+        description: 'Mandala radial azul con detalles amarillos.',
+        img: 'https://res.cloudinary.com/dhjlbf6xs/image/upload/v5678571633/MandalaApp/Mandalas/2_ijzggh.jpg',
+        price: 500,
+    },
+    {
+        id: 2,
+        categoryId: 1,
+        title: 'Multicolor Portal',
+        description: 'Mandala radial multicolor que evoca movimiento.',
+        img: 'https://res.cloudinary.com/dhjlbf6xs/image/upload/v1678571633/MandalaApp/Mandalas/4_cje6tk.jpg',
+        price: 800,
+    },
+    {
+        id: 3,
+        categoryId: 2,
+        title: 'Geometric Conflict',
+        description:
+            'Mandala geométrico con zonas radiales que muestra con colores, separaciones entre sus distintas zonas.',
+        img: 'https://res.cloudinary.com/dhjlbf6xs/image/upload/v1678571633/MandalaApp/Mandalas/7_wuv31m.jpg',
+        price: 1000,
+    },
+    {
+        id: 4,
+        categoryId: 2,
+        title: 'Kaleidoscopic Stained Glass',
+        description: 'Mandala radial que simula la estética de un vitral.',
+        img: 'https://res.cloudinary.com/dhjlbf6xs/image/upload/v1678571633/MandalaApp/Mandalas/8_e7q7nf.jpg',
+        price: 500,
+    },
+    {
+        id: 5,
+        categoryId: 1,
+        title: 'Forest Secret',
+        description: 'Mandala radial que evoca a los colores del bosque.',
+        img: 'https://res.cloudinary.com/dhjlbf6xs/image/upload/v1678571633/MandalaApp/Mandalas/9_vcptip.jpg',
+        price: 1000,
+    },
+    {
+        id: 6,
+        categoryId: 1,
+        title: 'Groovy Moons Clashing',
+        description:
+            'Mandalas radiales interceptados que evocan la imagen de cuerpos celestiales colisionando.',
+        img: 'https://res.cloudinary.com/dhjlbf6xs/image/upload/v1678571632/MandalaApp/Mandalas/3_ntybfx.jpg',
+        price: 1000,
+    },
+    {
+        id: 7,
+        categoryId: 1,
+        title: 'Christmas Star',
+        description:
+            'Mandala radial que evoca con sus colores rojos y verdes un ambiente navideño coronado por una estrella azul en su centro.',
+        img: 'https://res.cloudinary.com/dhjlbf6xs/image/upload/v1678571632/MandalaApp/Mandalas/5_taynah.jpg',
+        price: 800,
+    },
+    {
+        id: 8,
+        categoryId: 1,
+        title: 'Ying Yang Frenzy',
+        description:
+            'Mandala geométrico que representa el famoso símbolo del Ying Yang y lo complementa con vibrantes colores.',
+        img: 'https://res.cloudinary.com/dhjlbf6xs/image/upload/v1678571632/MandalaApp/Mandalas/1_kv2t5x.jpg',
+        price: 800,
+    },
+    {
+        id: 9,
+        categoryId: 1,
+        title: 'Fallen Leaves',
+        description:
+            'Mandala radial que representa con ricos colores verdes y amarillo a un grupo de hojas y flores que evocan sentimientos de paz con la naturaleza.',
+        img: 'https://res.cloudinary.com/dhjlbf6xs/image/upload/v1678571632/MandalaApp/Mandalas/6_hy3lrw.jpg',
+        price: 800,
+    },
+];
